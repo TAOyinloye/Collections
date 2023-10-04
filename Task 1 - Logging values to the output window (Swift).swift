42b4a3e614a176ee9512facadd47b3d2c3a25833
TAOyinloye <94188481+TAOyinloye@users.noreply.github.com>
2023-10-04 15:07:58 +0100

    Add files via upload

---
+++ Task 1 - Logging values to the output window (Swift).swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Task1
+//
+//  Created by Toyosi Oyinloye on 26/09/2023.
+//
+
+import Foundation
+
+var myInt = 7 //create variable int
+var myFloat: Float = 7.5 //create variable float
+var myString = "Hello"
+print(myInt)
+print(myFloat)
+print(myString)
+//To print the values on the same line (no new line), use a terminator
+print(myInt, terminator: "")
+print(myFloat, terminator: "")
+print(myString, terminator: "")
